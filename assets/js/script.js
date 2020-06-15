9e789de34ded888e02b5408417e09f33dc5b23de
juanruiz-cv <juanangelruiz.p@gmail.com>
2020-06-14 23:01:19 -0300

    menu desplegable

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,117 @@ function mostrarScroll(){
     }
 }
 window.addEventListener('scroll',mostrarScroll);
+
+/*Menu*/
+let boton = document.getElementById("btn-menu");
+let bnt1 = document.getElementById("1");
+let bnt2 = document.getElementById("2");
+let bnt3 = document.getElementById("3");
+let bnt4 = document.getElementById("4");
+let bnt5 = document.getElementById("5");
+let bnt6 = document.getElementById("6");
+let bnt7 = document.getElementById("7");
+let bnt8 = document.getElementById("8");
+let enlaces = document.getElementById("enlaces");
+let contador = 0;
+
+boton.addEventListener("click",function(){
+    if(contador == 0){
+        enlaces.className = ('enlaces dos');
+        contador=1;
+    }else{
+        enlaces.classList.remove('dos');
+        enlaces.className = ('enlaces uno');
+        contador = 0;
+    }
+})
+bnt1.addEventListener("click",function(){
+    if(contador == 0){
+        enlaces.className = ('enlaces dos');
+        contador=1;
+    }else{
+        enlaces.classList.remove('dos');
+        enlaces.className = ('enlaces uno');
+        contador = 0;
+    }
+})
+bnt2.addEventListener("click",function(){
+    if(contador == 0){
+        enlaces.className = ('enlaces dos');
+        contador=1;
+    }else{
+        enlaces.classList.remove('dos');
+        enlaces.className = ('enlaces uno');
+        contador = 0;
+    }
+})
+bnt3.addEventListener("click",function(){
+    if(contador == 0){
+        enlaces.className = ('enlaces dos');
+        contador=1;
+    }else{
+        enlaces.classList.remove('dos');
+        enlaces.className = ('enlaces uno');
+        contador = 0;
+    }
+})
+bnt4.addEventListener("click",function(){
+    if(contador == 0){
+        enlaces.className = ('enlaces dos');
+        contador=1;
+    }else{
+        enlaces.classList.remove('dos');
+        enlaces.className = ('enlaces uno');
+        contador = 0;
+    }
+})
+bnt5.addEventListener("click",function(){
+    if(contador == 0){
+        enlaces.className = ('enlaces dos');
+        contador=1;
+    }else{
+        enlaces.classList.remove('dos');
+        enlaces.className = ('enlaces uno');
+        contador = 0;
+    }
+})
+bnt6.addEventListener("click",function(){
+    if(contador == 0){
+        enlaces.className = ('enlaces dos');
+        contador=1;
+    }else{
+        enlaces.classList.remove('dos');
+        enlaces.className = ('enlaces uno');
+        contador = 0;
+    }
+})
+bnt7.addEventListener("click",function(){
+    if(contador == 0){
+        enlaces.className = ('enlaces dos');
+        contador=1;
+    }else{
+        enlaces.classList.remove('dos');
+        enlaces.className = ('enlaces uno');
+        contador = 0;
+    }
+})
+bnt8.addEventListener("click",function(){
+    if(contador == 0){
+        enlaces.className = ('enlaces dos');
+        contador=1;
+    }else{
+        enlaces.classList.remove('dos');
+        enlaces.className = ('enlaces uno');
+        contador = 0;
+    }
+})
+
+window.addEventListener('click',function(e){
+    console.log(e.target);
+    if(cerrado==false){
+        let span = document.querySelector('.links-header');
+        if(e.target == span){
+            contador=0;
+        }
+    }
+});
